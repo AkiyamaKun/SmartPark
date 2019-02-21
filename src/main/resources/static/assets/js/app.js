8b17a6664783487427a98289cd7c3c318bfd1d66
teramind244 <teramind244@gmail.com>
2019-02-21 17:48:52 +0700

    Remove unused import js file and add app.js

---
+++ src/main/resources/static/assets/js/app.js
@@ -0,0 +1,28 @@
+// Counter Number
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 3000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+function doAjax(url, method, callback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            let response = xhttp.responseText;
+            if (response && response.indexOf('error') > 0) {
+                error(response);
+            } else {
+                callback(response);
+            }
+        }
+    };
+    xhttp.open(method, url, true);
+    xhttp.send();
+}
