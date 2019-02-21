c10b734a572867042ab03cd47aee045799e3c961
teramind244 <teramind244@gmail.com>
2019-02-21 18:18:44 +0700

    Add error callback function for doAjax

--- src/main/resources/static/assets/js/app.js
+++ src/main/resources/static/assets/js/app.js
@@ -11,12 +11,12 @@ $('.count').each(function () {
     });
 });
 
-function doAjax(url, method, callback) {
+function doAjax(url, method, callback, error) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
             let response = xhttp.responseText;
-            if (response && response.indexOf('error') > 0) {
+            if (!response || (response && response.indexOf('error') > 0)) {
                 error(response);
             } else {
                 callback(response);
